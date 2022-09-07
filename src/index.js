17dc9193ba01cd5283ea378dd17544ffca0ded71
LeeJaeLee <cuonggaayf01@gmail.com>
2022-09-07 22:54:15 +0700

    Update 7/9/2022

--- src/index.js
+++ src/index.js
@@ -8,9 +8,60 @@ mixed.push(6);
 mixed.push(true);
 // any: Bất kì giá trị nào
 var age;
-// func
+// func basic
 var greet;
 greet = function () {
     console.log("hello");
 };
 greet();
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 6);
+var minus = function (a, b) {
+    return a + b;
+};
+var res = minus(10, 7);
+var logDetails = function (uid, item) {
+    console.log("".concat(item, " has a uid of ").concat(uid));
+};
+var greet1 = function (user) {
+    console.log("".concat(user.name, " says hello"));
+};
+// Func Signatures
+// let greet2: Function;
+//ex1:
+var gree;
+gree = function (name, greeting) {
+    console.log("".concat(name, " says ").concat(greeting));
+};
+//ex2:
+var calc;
+calc = function (numOne, numTwo, action) {
+    if (action === "add") {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
+};
+//ex3
+var logDetails1;
+logDetails1 = function (ninja) {
+    console.log("".concat(ninja.name, " id ").concat(ninja.age, " years old"));
+};
+// Dom & Type Casting
+var anchor = document.querySelector("a");
+// console.log(anchor?.href);
+var form = document.querySelector(".new-item-form");
+// console.log(form.children);
+var type = document.querySelector("#type");
+var toForm = document.querySelector("#toform");
+var details = document.querySelector("#details");
+var amount = document.querySelector("#amount");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log(type.value, toForm.value, details.value);
+});
--- src/index.ts
+++ src/index.ts
@@ -20,8 +20,98 @@ greet = () => {
 
 greet();
 
-const add = (a: number, b: number) => {
+const add = (a: number, b: number, c: number | string = 10) => {
   console.log(a + b);
+  console.log(c);
 };
 
 add(5, 6);
+
+const minus = (a: number, b: number) => {
+  return a + b;
+};
+
+let res = minus(10, 7);
+
+// Type Aliases
+type StringOrNum = string | number;
+type objWithName = { name: string; uid: StringOrNum };
+
+const logDetails = (uid: StringOrNum, item: string) => {
+  console.log(`${item} has a uid of ${uid}`);
+};
+
+const greet1 = (user: objWithName) => {
+  console.log(`${user.name} says hello`);
+};
+
+// Func Signatures
+// let greet2: Function;
+
+//ex1:
+let gree: (a: string, b: string) => void;
+gree = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
+};
+
+//ex2:
+let calc: (a: number, b: number, c: string) => number;
+calc = (numOne: number, numTwo: number, action: string) => {
+  if (action === "add") {
+    return numOne + numTwo;
+  } else {
+    return numOne - numTwo;
+  }
+};
+
+//ex3
+let logDetails1: (obj: { name: string; age: number }) => void;
+
+type person = { name: string; age: number };
+
+logDetails1 = (ninja: person) => {
+  console.log(`${ninja.name} id ${ninja.age} years old`);
+};
+
+// classes
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format() {
+    return `${this.client} owes @${this.amount} for ${this.details}`;
+  }
+}
+
+const invoice = new Invoice("emcuong", "ui doi oi", 200);
+
+let invoices: Invoice[] = [];
+invoices.push(invoice);
+
+console.log(invoices);
+
+// Public private && ReadOnly
+
+// Dom & Type Casting
+const anchor = document.querySelector("a");
+// console.log(anchor?.href);
+
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+// console.log(form.children);
+
+const type = document.querySelector("#type") as HTMLSelectElement;
+const toForm = document.querySelector("#toform") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  console.log(type.value);
+});
